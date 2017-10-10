ddd5d96f0fb5248a5397c7d635eb01492f46717e
JONGGON <medical18@naver.com>
2017-10-10 10:01:29 +0900

    upload

--- DeepHumanPrediction/Code/Master_Thesis/Human Motion Analysis/Motion Generation/main.py
+++ DeepHumanPrediction/Code/Master_Thesis/Human Motion Analysis/Motion Generation/main.py
@@ -12,7 +12,7 @@ time_step = 90
 seed_timestep = 30 # 0.6 second motion seed  / 2 second motion prediction
 batch_Frame = 1
 frame_time = 24
-save_period = 200000
+save_period = 60000
 parameter_shared = True # Parameters that determine whether or not the encoder decoder will share parameters
 
 '''Execution'''
@@ -24,7 +24,7 @@ if TEST :
 else:
 
     #batch learning
-    MotionNet(epoch=200000 , batch_size=102, save_period=save_period, cost_limit=0.001 ,
+    MotionNet(epoch=100000 , batch_size=102, save_period=save_period, cost_limit=0.001 ,
     optimizer='adam', learning_rate=0.0001 , lr_step=5000, lr_factor=0.99, stop_factor_lr=1e-08 , use_gpu=True ,
     TEST=TEST , num_layer=num_layer , cell=cell , hidden_unit=hidden_unit , time_step = time_step ,
     seed_timestep = seed_timestep , batch_Frame = batch_Frame , frame_time=frame_time , graphviz=True , parameter_shared=parameter_shared)
